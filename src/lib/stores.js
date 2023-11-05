10ea16a602dd39a3acc5077865d9333c99da1e68
Cameron Slipp <cdslipp@gmail.com>
2023-11-05 00:56:11 -0400

    Added pocketbase support and showMode store works

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,3 +2,5 @@ import { writable } from 'svelte/store';
 
 // Create a writable store with a default value
 export const rentalType = writable('Standard Rental');
+
+export const showMode = writable(false);
